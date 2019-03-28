15b6400732185b1e01b3f5e734c46275e8087ce8
alex volt <alexvolt@hotmail.com>
2019-03-28 17:32:03 -0400

    comm

--- ExpressoApi/Controllers/MenusController.cs
+++ ExpressoApi/Controllers/MenusController.cs
@@ -20,24 +20,24 @@ namespace ExpressoApi.Controllers
             _expressoDbContext = expressoDbContext;
         }
 
-        //[HttpGet]
-        //public IActionResult GetMenus()
-        //{
-        //    var menus = _expressoDbContext.Menus.Include("SubMenus");
+        [HttpGet]
+        public IActionResult GetMenus()
+        {
+            var menus = _expressoDbContext.Menus.Include("SubMenus");
 
-        //    return Ok(menus);
-        //}
+            return Ok(menus);
+        }
 
-        //[HttpGet("{id}")]
-        //public IActionResult GetMenus(int id)
-        //{
-        //    var menu = _expressoDbContext.Menus.Include("SubMenus").Where(m=>m.Id==id);
-        //    if (menu==null)
-        //    {
-        //        return NotFound();
-        //    }
+        [HttpGet("{id}")]
+        public IActionResult GetMenus(int id)
+        {
+            var menu = _expressoDbContext.Menus.Include("SubMenus").Where(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
 
-        //    return Ok(menu);
-        //}
+            return Ok(menu);
+        }
     }
 }
---
+++ ExpressoApi/Controllers/ReservationsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpressoApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpressoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        ExpressoDbContext _expressoDbContext;
+
+        public ReservationsController(ExpressoDbContext expressoDbContext)
+        {
+            _expressoDbContext = expressoDbContext;
+        }
+
+    }
+}
